c503926a502e4592c4ca08bb73b93a89fb71253b
jhonatantcn <42412295+jhonatantcn@users.noreply.github.com>
2021-02-11 00:20:19 -0300

    Criação da scene "Main" e novos assets
    --> Em Assets, na pasta da biblioteca "Mirror" foram excluídos os exemplos de jogos não utilizados neste projeto;
    --> A scene do jogo foi definida como "Pong";
    --> Foi criada a scene "Main" como tela inicial;
    --> Em Assets, foi criada a pasta "Graphics", contendo as subpastas "Fonts", "Materials", "PhysicsMaterials", "Prefabs", "Sprites" e "Textures";
    --> Ainda em Assets, foi criada a pasta "Sounds", contendo as subpastas "Music" e "SoundEffects";
    --> Foi criado o script "SceneLoader" para alternar entre as scenes;
    --> Foi criado o script "GameManager" até o momento para fechar o jogo;
    --> Bug: Quando acesso a scene "Pong" uma primeira vez, retorno a scene "Main" e acesso novamente "Pong" ocorre o seguinte: O gameObject "NetworkManager" é carregado em duplicidade e o jogo é finalizado.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Function to Quit Game
+#if UNITY_EDITOR // If inside the editor, just show the debug message "Quit Game!".
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game!");
+    }
+#else // If not, close the application.
+    public void QuitGame()
+    {
+        Application.Quit();
+        //System.Diagnostics.Process.GetCurrentProcess().Kill();
+    }
+#endif
+
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    // loadingScreen is a prefab to load next scene
+    public GameObject loadingScreen;
+
+    // Start is called before the first frame update
+    public void LoadScene(string sceneName)
+    {
+        Instantiate(loadingScreen);
+        StartCoroutine(LoadSceneAsync(sceneName));
+    }
+
+    // Start is called before the first frame update
+    private IEnumerator LoadSceneAsync(string sceneName)
+        {
+            var operation = SceneManager.LoadSceneAsync(sceneName);
+            while (!operation.isDone)
+            {
+                var progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+                loadingScreen.GetComponentInChildren<Slider>().value = progress; //Progresso da barra de carregamento
+                yield return null;
+            }
+        }
+}
